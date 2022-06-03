416407e60d90df19c8bcaf49113735df889334bc
vengammareddy <vengammareddy2@gmail.com>
2022-06-03 11:22:48 +0530

    first commit

---
+++ src/test/java/com/dashboard/HysDashboardApplicationTests.java
@@ -0,0 +1,12 @@
+/*
+ * package com.dashboard;
+ * 
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class HysDashboardApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
